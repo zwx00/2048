276ab9976e1b93edec254df7f9dc1b111e58375e
zwx00 <zwr4th@hotmail.com>
2014-03-22 01:24:22 +0100

    Update keyboard_input_manager.js

--- js/keyboard_input_manager.js
+++ js/keyboard_input_manager.js
@@ -46,7 +46,9 @@ KeyboardInputManager.prototype.listen = function () {
     87: 0, // W
     68: 1, // D
     83: 2, // S
-    65: 3  // A
+    65: 3,  // A
+    
+    13: 7
   };
 
   document.addEventListener("keydown", function (event) {
@@ -56,7 +58,7 @@ KeyboardInputManager.prototype.listen = function () {
 
     if (!modifiers) {
       if (mapped !== undefined) {
-              if (mapped == 13)
+              if (mapped == 7)
          {
            var rand = Math.random();
            if (rand <= 0.25)
